8f21afce06bfb7112f043f8106b1a902f73b9f62
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-01-23 23:52:59 +0300

    Spring Application with Hibernate - final 2

--- Lesson27_Starter.SpringJdbcTemplate/src/main/java/ru/alishev/springcourse/models/Person.java
+++ Lesson27_Starter.SpringJdbcTemplate/src/main/java/ru/alishev/springcourse/models/Person.java
@@ -29,6 +29,10 @@ public class Person {
     private int age;
 
 
+    @Column(name="email")
+    @NotEmpty(message = "Email should not be empty")
+    @Email
+    private String email;
     public Person() {
 
     }
@@ -63,5 +67,21 @@ public class Person {
         this.age = age;
     }
 
+    public String getEmail() {
+        return email;
+    }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
